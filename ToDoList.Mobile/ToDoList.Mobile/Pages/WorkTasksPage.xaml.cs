c4842abc2ad19565580dcffa5431a96c5abbf7f9
WojsoNcpu <72502103+WojsoNcpu@users.noreply.github.com>
2022-05-26 10:17:40 +0200

    Dodaj pliki projektów.

---
+++ ToDoList.Mobile/ToDoList.Mobile/Pages/WorkTasksPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ToDoList.Core;
+using Xamarin.Forms;
+
+namespace ToDoList.Mobile
+{
+    public partial class WorkTaskPage : ContentPage
+    {
+        public WorkTaskPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new WorkTasksPageViewModel();
+        }
+    }
+}
