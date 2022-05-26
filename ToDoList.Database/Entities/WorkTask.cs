aac74230f7acee38f59f62d71a4afee437f02c13
WojsoNcpu <72502103+WojsoNcpu@users.noreply.github.com>
2022-05-26 12:36:20 +0200

    Rozpoczęcie tworzenia kodu pod bazę danych

---
+++ ToDoList.Database/Entities/WorkTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToDoList.Database
+{
+    public class WorkTask
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
