6ee4d82415142c2560ec8145c111ea7fcce30618
WojsoNcpu <72502103+WojsoNcpu@users.noreply.github.com>
2022-05-26 10:54:52 +0200

    dodanie bazy danych do projektu

---
+++ ToDoList.Database/Entities/WorkTask.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Database
+{
+    public class WorkTask
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
