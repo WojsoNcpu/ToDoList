6a09b4ba83bc7c7393612f2b2abb34bb59ce2a56
WojsoNcpu <72502103+WojsoNcpu@users.noreply.github.com>
2022-06-16 09:12:32 +0200

    Rozpoczęcie pisania systemu do logowania

---
+++ ToDoList/Pages/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoList/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Logika interakcji dla klasy RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
